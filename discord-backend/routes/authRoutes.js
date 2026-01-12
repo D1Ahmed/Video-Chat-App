f416083e36e0786310bf447ba8d500c18eb1ae55
D1Ahmed <dawoodahmed653356@gmail.com>
2026-01-12 23:04:50 +0500

    jwt auth is working perf

--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -3,6 +3,8 @@ const router  =express.Router();
 const authControllers=require("../controllers/auth/authControllers");
 const Joi=require('joi');
 const validator=require('express-joi-validation').createValidator({});
+const auth=require('../middleware/auth');
+
 const registerSchema =Joi.object({
     username: Joi.string().min(3).max(12).required(),
     password: Joi.string().min(6).max(12).required(),
@@ -17,5 +19,11 @@ const loginSchema = Joi.object({
 router.post("/register",validator.body(registerSchema), authControllers.controllers.postRegister);
 
 router.post("/login",validator.body(loginSchema), authControllers.controllers.postLogin);
+
+router.get("/test",auth, (req,res)=>
+{
+    res.send("request passed");
+}); 
+
 
 module.exports=router; 
